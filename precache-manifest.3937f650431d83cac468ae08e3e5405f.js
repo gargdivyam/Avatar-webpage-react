a5d4e5bbf96d00742f34b3798ff6bf24be5e7ce2
gargdivyam327 <gargdivyam327@gmail.com>
2019-12-12 22:47:52 +0530

    Updates

---
+++ precache-manifest.3937f650431d83cac468ae08e3e5405f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "135486aef1b0a73d23dbb2485bbc09cf",
+    "url": "/Avatar-webpage-react/index.html"
+  },
+  {
+    "revision": "7d0b6cdaae18d0d899a8",
+    "url": "/Avatar-webpage-react/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "dcc6ae73105f2313718a",
+    "url": "/Avatar-webpage-react/static/css/main.4772b139.chunk.css"
+  },
+  {
+    "revision": "7d0b6cdaae18d0d899a8",
+    "url": "/Avatar-webpage-react/static/js/2.9f94a17a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Avatar-webpage-react/static/js/2.9f94a17a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dcc6ae73105f2313718a",
+    "url": "/Avatar-webpage-react/static/js/main.1f8bbb51.chunk.js"
+  },
+  {
+    "revision": "677ea4a463082fe2baf4",
+    "url": "/Avatar-webpage-react/static/js/runtime-main.6b3a6912.js"
+  }
+]);
